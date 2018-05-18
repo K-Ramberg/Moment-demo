9a2cf5d4c3785a5aa182efc26b40cda4d818f9dd
K-Ramberg <kramberg0350@gmail.com>
2018-05-17 20:06:31 -0400

    adding more time examples

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-var now = moment()
+$(() => {
+    let thenTime = moment().utc()
+    let nowP = moment.utc()
+    let deliverTime = moment().add(5, 'day')
+    let funTime = moment().utcOffset(80)
 
-$("#now").text(now.format('MMMM Do YYYY, h:mm:ss a'))
+$("#now").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
+$("#then").text(thenTime.format('MMMM Do YYYY, h:mm:ss a'))
+$('#deliverFuture').text(deliverTime.format('MMMM Do YYYY, h:mm:ss a'))
+$('#nowParseable').text(nowP.format('DD/MM/YYYY, h:mm:ss'))
+
+
+
+});
